aafe560253f643d3f677d8330e79d1434dc940c8
Ali Al-Shemery <ali@ashemery.com>
2015-03-25 20:00:08 +0200

    Updated to New Release
    Black Hat Asia 2015 release v1.0

--- uyr.py
+++ uyr.py
@@ -1,61 +1,142 @@
 #!/usr/bin/python
 #coding=utf-8
+__author__ = 'binary'
 
-from uyr import uyr_hide
-from uyr import uyr_extract
+# Loading the required modules
 import argparse
 import sys
+import os
+#import zlib
+from uyr import UYRLSB
+from uyr import UYRStepic
+from uyr import UYRUtils
 
-def banner():
-    print """
-#####################################################################################
-# _____         _              __ __                   _____         _              #
-# |  |  | ___  _| | ___  ___   |  |  | ___  _ _  ___   | __  | ___  _| | ___  ___   #
-# |  |  ||   || . || -_||  _|  |_   _|| . || | ||  _|  |    -|| .'|| . || .'||  _|  #
-# |_____||_|_||___||___||_|      |_|  |___||___||_|    |__|__||__,||___||__,||_|    #
-#                                                                                   #
-#####################################################################################
-#                                                                                   #"
-#   UYR is an exfiltration and covert channel application                           #"
-#   (1) Communicate with your friend in a stealthy manner                           #"
-#   (2) Exfiltrate some data out of a network in control                            #"
-#                                                                                   #"
-#####################################################################################
-\n"""
+version = '1.0'
 
 def Main():
-    #banner()
+    print UYRUtils.uyr_banner()
 
     parser = argparse.ArgumentParser(description='Welcome to Under Your Radar (UYR)')
-    group = parser.add_mutually_exclusive_group()
-
-    group.add_argument('-e', "--extract", help="Extract Message from image", action="store_true")
-    group.add_argument('-m', '--hide-msg', help="Hide Message in image", action="store_true")
-    group.add_argument('-f', '--hide-file', help="Hide Text File in image", action="store")
-    parser.add_argument('--image', help="Image file used to hide your message", action="store")
-    parser.add_argument('--video', help="Video file used to hide your message", action="store")
-
+    parser.add_argument('-t', '--tweet', help="tweet your data", action="store", dest="tweet")
+    parser.add_argument('-o', '--output', help='output file name', action="store", dest="output")
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+    hidegroup = parser.add_argument_group("hiding Operations")
+    hgroup = hidegroup.add_mutually_exclusive_group()
+    hgroup.add_argument('-m', '--hide-msg', help="hide message in image", action="store_true", dest="hmsg")
+    hgroup.add_argument('-f', '--hide-file', help="hide text file in image", action="store", dest="htfile")
+    hgroup.add_argument('-b', '--hide-bin', help="hide binary file in image", action="store", dest="hbfile")
+    extractgroup = parser.add_argument_group("extracting Operations")
+    egroup = extractgroup.add_mutually_exclusive_group()
+    egroup.add_argument('-e', "--extract", help="extract message from image", action="store", dest="emsg")
+    egroup.add_argument('-x', '--extract-bin', help="extract binary file from image", action="store", dest="ebfile")
+    ivfgroup = parser.add_argument_group("image & video files to be used")
+    ivfgroup.add_argument('--image', help="image file used for hiding", action="store", dest="image")
+    ivfgroup.add_argument('--video', help="video file used for hiding/extracting", action="store", dest="video")
     args = parser.parse_args()
-    print parser.parse_args()
+    #print parser.parse_args()
 
-    path2Image = args.image
-    path2Video = args.video
+    # Doing some cleaning and checking:
+    # Clearing the tmp directory before proceeding
+    if os.path.exists('tmp'):
+        UYRUtils.cleanTmpDir()
 
-    if args.extract:
-        uyr_extract.extractMSG(path2Image, path2Video)
-    elif args.hide_msg:
-        print "Performing operation: Hide Message"
-        secretMessage = raw_input("Please enter your secret message: ")
-        uyr_hide.hideMSG(secretMessage, path2Video, path2Image)
-    elif args.hide_file:
-        print "Performing operation: Hide Text File"
-        hiddenFileName = args.hide_file
-        with open(hiddenFileName, 'r') as hiddenFile:
-            secretData = hiddenFile.read().strip('\n\r')
-        uyr_hide.hideMSG(secretData, path2Video, path2Image)
-    else:
-        sys.exit()
+    # Prepare the tmp directory for processing
+    UYRUtils.createTMPDir()
+
+    # Message / Data Hiding
+    if args.hmsg or args.htfile or args.hbfile:
+        # Reading the Image and Video Files
+        path2Image = args.image
+        path2Video = args.video
+        hideBinFile = args.hbfile
+        hideTxTFile = args.htfile
+        hideMsg = args.hmsg
+
+        if not UYRUtils.checkPath2Image(path2Image):
+            print "[+] Sorry image file does not exist"
+            sys.exit()
+        if not UYRUtils.checkPath2Video(path2Video):
+            print "[+] Sorry video file does not exist"
+            sys.exit()
+
+        # Method to be performed on hiding a message
+        if hideMsg:
+            print "[+] Performing Operation: Hide Message"
+            secretMessage = raw_input("[+] Enter your secret message: ")
+            charmap = UYRLSB.hideMessage(secretMessage, path2Video, path2Image)
+            print "[+] Hidden successfully!!!"
+            print "[+] Your character map is: ", charmap
+
+        # Method to be performed on hiding a txt file
+        if hideTxTFile:
+            print "[+] Performing Operation: Hide Text File"
+            with open(hideTxTFile, 'r') as hiddenFile:
+                secretData = hiddenFile.read().strip('\n\r')
+            charmap = UYRLSB.hideMessage(secretData, path2Video, path2Image)
+            print "[+] Hidden successfully!!!"
+            print "[+] Your character map is: ", charmap
+
+        # Method to be performed to hide a binary file
+        if hideBinFile:
+            print "[+] Performing Operation: Hiding Binary File"
+            f = open(hideBinFile, 'rb')
+            binaryContent = f.read()
+            f.close()
+            #zipSecretMessage = zlib.compress(binaryContent)
+            #imgNamePNG = UYRStepic.hideMessage(zipSecretMessage, path2Video, path2Image)
+            charmap = UYRStepic.hideMessage(binaryContent, path2Video, path2Image)
+            print "[+] Hidden successfully!!!"
+            print "[+] Your character map is: ", charmap
+
+    # Message / Data Extraction
+    if args.emsg or args.ebfile:
+        path2Video = args.video
+        extractMsg = args.emsg
+        extractBinFile = args.ebfile
+
+        # Method to be performed on message extraction
+        if extractMsg:
+            path2Image = args.emsg
+            print "[+] Performing Operation: Extract Message"
+            secMessage = UYRLSB.extractMSG(path2Image, path2Video)
+            print "[+] Message extracted successfully!!!"
+            print "[+] The secret message is: ", secMessage
+
+        # Method to be performed to extract a binary file
+        if extractBinFile:
+            fName = args.output
+            if not args.output:
+                print "[+] Sorry you must provide a name for the output file!"
+                sys.exit()
+
+            path2Image = args.ebfile
+            print "[+] Performing Operation: Extract Binary File"
+            f2 = open(fName, 'wb+')
+            #zippedBinaryData = UYRStepic.extractMSG(path2Image, path2Video)
+            binaryFile = UYRStepic.extractMSG(path2Image, path2Video)
+            #binaryFile = zlib.decompress(zippedBinaryData)
+            #f2.write(zippedBinaryData)
+            f2.write(binaryFile)
+            f2.close()
+            print "[+] Data extracted successfully!!!"
+            print "[+] Extracted file named: %s" % fName
+            print "[+] The binary file is: ", len(binaryFile), "bytes long"
+
+    # Sending charmaps as tweets :)
+    if args.tweet:
+        txtTweet = unicode(raw_input("[+] Enter the required tweet message: "))
+        print "[+] Caution: max file size twitter can handle is 5Mb"
+        path2TweetImage = args.tweet
+
+        configParser = UYRUtils.loadConfigFile('socialnetworks.ini')
+        twitterInfo = UYRUtils.loadTwitterConfig(configParser)
+        UYRUtils.tweetWithImage(twitterInfo, txtTweet, path2TweetImage)
+        print "[+] Tweet sent successfully!!!"
+
+    # Cleaning and exiting
+    UYRUtils.cleanTmpDir()
+    print "[+] UYR Finished Successfully!!!"
+    sys.exit()
 
 if __name__ == '__main__':
     Main()
-
